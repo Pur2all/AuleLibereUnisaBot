0c9091183e681275f79fa200fc8d7970d3bdff10
Pur2all <39830676+Pur2all@users.noreply.github.com>
2019-11-28 15:54:53 +0100

    Uploaded files

---
+++ AuleLibereBot.py
@@ -0,0 +1,71 @@
+import telebot
+from FreeRoom import *
+
+TOKEN = "783944951:AAHdmcNjyI-8fZsSKv_-hIMp5MLeBkbDUvc"
+
+
+def make_buildings_keyboard_markup():
+    markup = telebot.types.ReplyKeyboardMarkup()
+    buttons = []
+
+    for key in buildings:
+        temp_button = telebot.types.KeyboardButton(text=key)
+        buttons.append(temp_button)
+    markup.add(*buttons)
+
+    return markup
+
+
+def make_classroom_keyboard_markup(building):
+    markup = telebot.types.ReplyKeyboardMarkup()
+    buttons = []
+    values = rooms_for_buildings[building]
+
+    for value in values:
+        temp_button = telebot.types.KeyboardButton(value)
+        buttons.append(temp_button)
+    markup.add(*buttons)
+
+    return markup
+
+
+setup()
+bot = telebot.TeleBot(TOKEN)
+building: str = None
+
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Bla bla bla")
+
+
+@bot.message_handler(commands=["edifici"])
+def print_buildings_keyboard(message):
+    markup = make_buildings_keyboard_markup()
+
+    bot.send_message(message.chat.id, "Scegli un edificio:", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: message.text in buildings)
+def print_rooms_keyboard(message):
+    global building
+
+    building = message.text
+    markup = make_classroom_keyboard_markup(buildings[building])
+
+    bot.send_message(message.chat.id, "Scegli un'aula:", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: message.text in rooms_for_buildings[buildings[building]])
+def print_free_hours(message):
+    free_times = extract_free_time(get_all_rooms_events(building)[message.text])
+
+    format_string = "L'aula è libera nei seguenti orari:\n"
+
+    for time in free_times:
+        format_string += "- Dalle " + time[0] + " alle " + time[1] + "\n"
+
+    bot.send_message(message.chat.id, format_string, reply_markup=telebot.types.ReplyKeyboardRemove())
+
+
+bot.polling(none_stop=False)
---
+++ FreeRoom.py
@@ -0,0 +1,101 @@
+import datetime
+import requests
+import json
+from collections import defaultdict
+
+buildings = {
+    "Aule Virtuali": "VIRT",
+    "Campus Baronissi": "BCORPOA0-C0-D1-L1",
+    "A1": "FRE-RATO",
+    "A2": "FAUL-MAG",
+    "B": "FSTEC-01",
+    "B1": "FINV-01A",
+    "B2": "FINV-02B",
+    "C": "FSTEC-02-03",
+    "C1": "FINV-03C",
+    "C2": "FINV-04D",
+    "D": "FSTEC-04",
+    "D1": "FINV-05E",
+    "D2": "FINV-06A",
+    "D3": "FINV-08C",
+    "E": "FSTEC-05-06",
+    "E1": "FINV-09C",
+    "E2": "FINV-07E",
+    "F": "FSTEC-07-09",
+    "F1": "FINV-11C",
+    "F2": "FINV-12B",
+    "F3": "FINV-13C",
+    "I1": "FL-INGSA",
+    "L2": "FL-ING03"
+}
+
+rooms_for_buildings = defaultdict(list)
+
+url = "https://easycourse.unisa.it/AgendaStudenti//rooms_call.php"
+
+
+def setup():
+    global rooms_for_buildings
+
+    response = requests.get(url)
+    area_rooms = json.loads(response.content.decode("utf8"))["area_rooms"]
+
+    for key, value in area_rooms.items():
+        for room_id, data in value.items():
+            rooms_for_buildings[key].append(data["room_name"])
+
+
+def format_time(list_of_times):
+    len_of_list = len(list_of_times)
+
+    for i in range(len_of_list):
+        start, end = list_of_times[i][0], list_of_times[i][1]
+        list_of_times[i] = ("{:02d}:{:02d}".format(start.hour, start.minute),
+                            "{:02d}:{:02d}".format(end.hour, end.minute))
+
+    return list_of_times
+
+
+def extract_free_time(list_of_full_hours):
+    assert list_of_full_hours is not None
+
+    list_of_free_hours = []
+
+    today = datetime.datetime.today()
+    start_of_the_day = datetime.datetime(today.year, today.month, today.day, hour=8, minute=30)
+    end_of_the_day = datetime.datetime(today.year, today.month, today.day, hour=19, minute=0)
+
+    list_of_full_hours.insert(0, (None, start_of_the_day))
+    list_of_full_hours.append((end_of_the_day, None))
+
+    len_of_list = len(list_of_full_hours)
+    i = 0
+
+    while i < len_of_list - 1:
+        next_occupied_from, this_occupied_to = list_of_full_hours[i + 1][0], list_of_full_hours[i][1]
+
+        if next_occupied_from - this_occupied_to != datetime.timedelta(0):
+            list_of_free_hours += [(this_occupied_to, next_occupied_from)]
+
+        i += 1
+
+    return format_time(list_of_free_hours)
+
+
+# Se vuoi farlo per un aula singola il parametro da mettere è quello dell'ID del JSON, ad esempio P11 è FINV-13CP01003
+def get_all_rooms_events(of_building=None):
+    assert of_building is not None
+
+    global response
+
+    response = requests.get(url, params={"sede": buildings[of_building]})
+    events = json.loads(response.content.decode("utf8"))["events"]
+
+    room_events = defaultdict(list)
+
+    for event in events:
+        occupied_from, occupied_to = datetime.datetime.fromtimestamp(event["timestamp_from"]), \
+                                         datetime.datetime.fromtimestamp(event["timestamp_to"])
+        room_events[event["NomeAula"]] += [(occupied_from, occupied_to)]
+
+    return room_events
